64d82a179573842012479080340088376d2e5687
Mauricio Robayo <rfmajo@gmail.com>
2019-10-30 14:56:11 -0500

    Add sass-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import "./style.css";
+import "./style.scss";
 
 document.querySelector("#content").textContent = "Hello, world!";
--- webpack.config.js
+++ webpack.config.js
@@ -24,15 +24,8 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader"
-          }
-        ]
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   }

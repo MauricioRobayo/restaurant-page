655ad5a95b31325c15b30968faf69b791e968b0f
Mauricio Robayo <rfmajo@gmail.com>
2019-10-30 15:12:19 -0500

    Include contentHash on webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main.[contentHash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],

576fe58b03d8173269f62dfd26ae42728c7e43c8
Mauricio Robayo <hi@mauriciorobayo.com>
2020-01-18 09:33:37 -0500

    refactor: reduce complexity of contact function

--- src/js/contact.js
+++ src/js/contact.js
@@ -18,35 +18,53 @@ function buildLocation(location) {
   return fragment
 }
 
+function contactImg(siteInfo) {
+  return createElement('img', {
+    classes: ['img-rounded'],
+    src: siteInfo.picture.large,
+  })
+}
+
+function contactLocation(siteInfo) {
+  const location = createElement('div', {
+    id: 'contact-location',
+  })
+  location.appendChild(buildLocation(siteInfo.location))
+  return location
+}
+
+function contactPhone(siteInfo) {
+  return createElement('p', {
+    textContent: siteInfo.phone,
+  })
+}
+
+function contactEmail(siteInfo) {
+  return createElement('p', {
+    textContent: siteInfo.email,
+  })
+}
+
+function contactName(siteInfo) {
+  return createElement('p', {
+    id: 'contact-name',
+    textContent: Object.values(siteInfo.name).join(' '),
+  })
+}
+
 function contact(siteInfo) {
   const container = createElement('div', {
     id: 'contact',
   })
 
-  const image = createElement('img', {
-    classes: ['img-rounded'],
-    src: siteInfo.picture.large,
-  })
+  container.append(
+    contactImg(siteInfo),
+    contactName(siteInfo),
+    contactLocation(siteInfo),
+    contactEmail(siteInfo),
+    contactPhone(siteInfo)
+  )
 
-  const name = createElement('p', {
-    id: 'contact-name',
-    textContent: Object.values(siteInfo.name).join(' '),
-  })
-
-  const email = createElement('p', {
-    textContent: siteInfo.email,
-  })
-
-  const phone = createElement('p', {
-    textContent: siteInfo.phone,
-  })
-
-  const location = createElement('div', {
-    id: 'contact-location',
-  })
-
-  location.appendChild(buildLocation(siteInfo.location))
-  container.append(image, name, location, email, phone)
   return container
 }
 export default contact
